0eccd5c273281c984848c89dfefded00779191c3
BK Box <bk@theboxes.org>
2013-02-22 09:37:05 -0600

    One variable to share

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,9 @@ default["graphite"]["version"] = "0.9.10"
 
 default["graphite"]["local_source"] = "/usr/local/src"
 
-default["whisper"]["source"] = "https://github.com/downloads/graphite-project/whisper/whisper-0.9.10.tar.gz"
-default["carbon"]["source"] = "https://github.com/downloads/graphite-project/carbon/carbon-0.9.10.tar.gz"
-default["graphite"]["source"] = "https://github.com/downloads/graphite-project/graphite-web/graphite-web-0.9.10.tar.gz"
+default["whisper"]["source"] = "https://github.com/downloads/graphite-project/whisper/whisper-#{node['graphite']['version']}.tar.gz"
+default["carbon"]["source"] = "https://github.com/downloads/graphite-project/carbon/carbon-#{node['graphite']['version']}.tar.gz"
+default["graphite"]["source"] = "https://github.com/downloads/graphite-project/graphite-web/graphite-web-#{node['graphite']['version']}.tar.gz"
 
 default["whisper"]["dependencies"] =  ["python"]
 default[:carbon][:dependencies] = ["python", "python-twisted"]
